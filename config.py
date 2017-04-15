f59512826bdc2b568b58b2d32f88859ca9a1a733
Сергей Нещадим <raser233@gmail.com>
2017-04-15 15:16:43 +0700

    Nice

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 token = '373012166:AAGTiDecGA8bK3gSCdmZurObzpzEFv5QSK0'
 start_message = 'Добрый день! Напишите, что вас интересует.'
+get_message = 'Ваше сообщение отправлено на обработку.'
+unimportant_messages = ['да','нет','привет']
+stickers_id = ['CAADAwADTgYAAmFKuQABl6fOzoLfppQC','CAADAwADUgYAAmFKuQABdV1GwOfhYF4C','CAADAwADqwYAAmFKuQABs7tYYEdF8_oC','CAADAwADnQYAAmFKuQABJs_HCy9dQX8C']
+emotes = ['😂','😁','😆']
+prediction_message = 'Вас интересует %s. Да?'
--- index.py
+++ index.py
@@ -1,11 +1,61 @@
 import telebot
 import config
+import re
+import random
+
+secure_random = random.SystemRandom()
+remove_whitespace = re.compile('/^\s*([\S\s]*?)\s*$/')
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands = ['start'])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id,config.start_message)
+
+@bot.message_handler(content_types=["text"] )
+def text_handler(message):
+    message_text = norm_text(message.text)
+
+    if message_text in config.unimportant_messages:
+        reaction_on_unimp_messages(message)
+    else:
+        bot.send_message(message.chat.id, config.get_message)
+        bot.send_message(message.chat.id,config.prediction_message % prediction(message_text))
+
+@bot.message_handler(content_types=["sticker"] )
+def handle_sticker(message):
+    pass
+
+def prediction(text):
+    return "банкоматы"
+
+def reaction_on_unimp_messages(message):
+    r = secure_random.random()
+    if(r >= 0.5):
+        bot.send_sticker(message.chat.id, secure_random.choice(config.stickers_id))
+    else:
+        bot.send_message(message.chat.id,secure_random.choice(config.emotes))
+def norm_text(text):
+    r = re.compile(' /  +/g')
+    start_ind = 0
+    for i in range(len(text)):
+        if text[i] == ' ':
+            start_ind +=1
+        else:
+            break
+    revt = text[::-1]
+    end_ind = len(text)
+    for i in range(len(text)):
+        if revt[i] == ' ':
+            start_ind -= 1
+        else:
+            break
+    res = text[start_ind:end_ind]
+    while '  ' in res:
+        res = res.replace('  ',' ')
+    return res
+
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
